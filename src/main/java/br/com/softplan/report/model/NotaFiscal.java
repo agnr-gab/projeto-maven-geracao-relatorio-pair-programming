19d34ec0012f56967ad8c03b1a04af914f7c20f5
Aretuza Nascimento <aretuza.gab@gmail.com>
2024-02-21 19:37:27 -0300

    refactor: add toString

--- src/main/java/br/com/softplan/report/model/NotaFiscal.java
+++ src/main/java/br/com/softplan/report/model/NotaFiscal.java
@@ -4,20 +4,24 @@ import java.math.BigDecimal;
 
 public class NotaFiscal {
 
-	private final Long numero;
-	private final BigDecimal valor;
-	
-	public NotaFiscal(Long numero, BigDecimal valor) {
-		this.numero = numero;
-		this.valor = valor;
-	}
+    private final Long numero;
+    private final BigDecimal valor;
 
-	public Long getNumero() {
-		return numero;
-	}
+    public NotaFiscal(Long numero, BigDecimal valor) {
+        this.numero = numero;
+        this.valor = valor;
+    }
 
-	public BigDecimal getValor() {
-		return valor;
-	}
-	
+    public Long getNumero() {
+        return numero;
+    }
+
+    public BigDecimal getValor() {
+        return valor;
+    }
+
+    @Override
+    public String toString() {
+        return numero.toString() + " cujo valor é R$ " + getValor();
+    }
 }
